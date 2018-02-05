d908163a02af9d925b89e1b16fda23b0fc52584a
Adrian <b.beracho@gmail.com>
2018-02-05 14:13:38 -0400

    Crea test de parametros

---
+++ test/integracion/parametros/parametro.int.js
@@ -0,0 +1,42 @@
+/**
+* Tests de integración para Parámetros
+*/
+'use strict';
+const request = require('supertest');
+const should = require('should');
+const sequelize = require('sequelize');
+
+let token = '';
+let usuarioCreado = {};
+const user = { usuario: "inscripciones", contrasena: "Developer"};
+
+describe('====================== APIREST DPA ======================', () => { // nos autenticamos inicialmente
+  before((done) => {
+    request(server)
+      .post('/autenticar')
+      .send(user)
+      .end((err, res) => {
+        const result = res.body;
+        token = result.token;
+        done();
+      });
+  });
+
+  // Ruta: /api/v1/parametrosRude
+  it('>>> Devuelve todos los parámetros para regro RUDE', (done) => {
+    request(server)
+      .get(`/api/v1/parametrosRude`)
+      .set('Authorization', `Bearer ${token}`)
+      .expect(200)
+      .end((err, res) => {
+        if (err) {
+          return done(err);
+        }
+        should(res.body.finalizado).be.ok;
+        should(res.body.mensaje).be.ok;
+        should(res.body.datos).be.ok;
+        res.body.finalizado.should.be.equal(true);
+        done();
+      });
+  });
+});
