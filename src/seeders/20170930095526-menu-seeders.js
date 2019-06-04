3de5caeddab7643cdaadfbbdaa6ccc2e51015491
Adrian <b.beracho@gmail.com>
2019-06-04 08:42:15 -0400

    inactiva menu de registro semestral

--- src/seeders/20170930095526-menu-seeders.js
+++ src/seeders/20170930095526-menu-seeders.js
@@ -463,12 +463,12 @@ module.exports = {
         method_post: true,
         method_put: true,
         method_delete: false,
-        estado: 'ACTIVO',
+        estado: 'INACTIVO',
         _usuario_creacion:1,
         _fecha_creacion: new Date(),
         _fecha_modificacion: new Date(),
         fid_menu_padre: 11,
-        visible: true,
+        visible: false,
       },
       {
         // 27
