4cedb16bb7636455b53c414351753fd077889536
Adrian Marcelo Berazain Mallea <marcelo.berazain@gmail.com>
2020-01-21 18:21:30 -0400

    Verifica existencia de persona con CI

--- src/routes/inscripcion/personaRT.js
+++ src/routes/inscripcion/personaRT.js
@@ -12,6 +12,12 @@ module.exports = app => {
   //   .then(respuesta => Util.mensajeExito(res, "Obtención de datos exitosa.", 200, respuesta))
   //   .catch(error => Util.mensajeError(res, error.message));
   // });
+  app.route("/api/v1/existePersona")
+  .get((req, res) => {
+    personaBL.buscaPersona(req.query, req.body, models)
+    .then(respuesta => Util.mensajeExito(res, "Obtención de datos exitosa.", 200, respuesta))
+    .catch(error => Util.mensajeError(res, error.message));
+  });
 
   app.route("/api/v1/actualizaPersona")
   .put((req, res) => {
